eee0858ce3db654eb140f6822c390899f86361fc
PokSoul <saiyudmeechai@gmail.com>
2020-12-12 13:15:24 +0100

    add article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+# L’utilisateur doit pouvoir garder un mot de passe sécurisé.
+  has_secure_password
+
+  # L’email ne doit pas être nul, et doit être unique dans toute la base.
+  validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :articles
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get    '/sign_in',  to: 'sessions#sign_in_form'
+  post   '/sign_in',  to: 'sessions#sign_in'
+  delete '/sign_out', to: 'sessions#sign_out'
+
 end
